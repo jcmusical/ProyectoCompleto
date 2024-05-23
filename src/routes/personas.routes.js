649b21c6d1f5269de84a9b33bf94572c10fbe5c8
jcmusical <jcmusical@gmail.com>
2024-05-22 21:26:39 -0500

    Creamos la vista add y la ruta y el post del add, se actualizaron routes.js y add.hbs

--- src/routes/personas.routes.js
+++ src/routes/personas.routes.js
@@ -7,6 +7,19 @@ router.get('/add', (req, res)=>{
     res.render('personas/add')
 });
 
+router.post('/add', async (req, res)=>{
+    try {
+        const { name, lastname, age} = req.body
+        const newPersona = {
+            name, lastname, age
+        }
+        await pool.query('INSERT INTO personas SET ?', [newPersona]);
+        res.redirect('/list');
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 router.get('/list', async(req, res) => {
     /* [result] pq array */
     try {
